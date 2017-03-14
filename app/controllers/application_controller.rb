c3d4d85dd2269a6fd149b945211face76b625c25
M Rezaur Rahman <mrezaurrahman@live.com>
2017-03-14 21:02:55 +0600

    protedted from forgery

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,9 @@
 class ApplicationController < ActionController::Base
+  #this line is for development purpose
+  # this line is for development purpose
+  # protect_from_forgery with: :null_session
+ 
+  #the following line should be uncommented in production
+  # the following line should be uncommented in production
   protect_from_forgery with: :exception
 end
